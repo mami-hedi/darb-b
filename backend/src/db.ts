98e500cdb8de2b49ed957ff3ffb4b74b1806766e
mami-hedi <hedimemi@gmail.com>
2026-02-02 23:14:26 +0100

    Refactor DB connection to use environment variables
    Updated database connection configuration to use environment variables for host, user, password, database, and port. Added SSL configuration for Aiven.

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,14 +1,20 @@
 import mysql from "mysql2/promise";
-
 import dotenv from "dotenv";
+
 dotenv.config();
 
 export const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "darseifboukhris",
+  // Si process.env.DB_HOST existe (sur Render), il l'utilise, sinon il prend "localhost"
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_NAME || "darseifboukhris",
+  port: process.env.DB_PORT || 3306, // Aiven utilise un port différent de 3306
+  
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
+
+  // AJOUT INDISPENSABLE POUR AIVEN (SSL)
+  ssl: process.env.DB_HOST ? { rejectUnauthorized: false } : null
 });
