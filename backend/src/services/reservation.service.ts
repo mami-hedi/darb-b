eb983ab2d333b20978b5aa26a208e62023740f90
mami-hedi <hedimemi@gmail.com>
2026-02-03 13:38:13 +0100

    Mise à jour pending en attente

--- backend/src/services/reservation.service.ts
+++ backend/src/services/reservation.service.ts
@@ -44,8 +44,8 @@ export const createReservation = async (reservation: Reservation) => {
   const [result]: any = await db.query(
     `INSERT INTO reservations 
      (room_id, name, email, phone, checkin, checkout, message, status)
-     VALUES (?, ?, ?, ?, ?, ?, ?, 'pending')`,
-    [room_id, name, email, phone || null, checkin, checkout, message || null]
+     VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
+    [room_id, name, email, phone || null, checkin, checkout, message || null, 'en attente']
   );
 
   return result.insertId;
